f446a9dbceb8d0c1fe8cbe09b487f9ae8d314e24
graph226 <graph226@gmail.com>
2016-01-28 17:29:16 +0900

    add function
    modified function

--- SearchWord.py
+++ SearchWord.py
@@ -4,11 +4,6 @@ import tweepy
 api = twpy.api
 query = "ガルパン"
 
-# search limit is maybe 200
-for tweet in tweepy.Cursor(api.search,q=query).items(100):
-    #print(tweet.created_at, tweet.user.screen_name, tweet.text)
-    print(tweet.text)
-
 def getTweet(query,limit=100):
     result = []
     for tweet in tweepy.Cursor(api.search,q=query).items(limit):
